75c44242de399bb88705fde4efec3a465c0c6a12
Michael Maurer <3191110276@users.noreply.github.com>
2021-12-20 11:04:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -185,7 +185,7 @@ resource "kubernetes_deployment" "metrics-server" {
         container {
           name = "metrics-server"
           
-          image             = "k8s.gcr.io/metrics-server/metrics-server:v0.4.2"
+          image             = "k8s.gcr.io/metrics-server/metrics-server:v0.5.2"
           image_pull_policy = "IfNotPresent"
           
           args = ["--cert-dir=/tmp", "--secure-port=4443", "--kubelet-preferred-address-types=InternalIP", "--kubelet-use-node-status-port", "--kubelet-insecure-tls"]
